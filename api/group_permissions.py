b7386d7c784cac9c5875aa4b65025cffe5d66781
Adam Chiu <zyx112388@hotmail.com>
2021-03-14 23:05:25 -0400

    Fixed anonymous user seeing error in image api

--- api/group_permissions.py
+++ api/group_permissions.py
@@ -33,5 +33,8 @@ class IsUserSelf(permissions.BasePermission):
 
 
 class IsUserSelfOrAdmin(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated
+
     def has_object_permission(self, request, view, obj):
         return obj.owner == request.user or request.user.is_staff
